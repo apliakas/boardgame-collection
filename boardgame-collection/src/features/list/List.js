554eaf208070651f3de5c96c7623ce7bf1074c29
Alex Pliakas <pliakasbenitez@gmail.com>
2021-02-23 15:33:39 +0100

    delete function and styles added and working

--- boardgame-collection/src/features/list/List.js
+++ boardgame-collection/src/features/list/List.js
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addBoardgame } from './listSlice'
+import { addBoardgame, deleteBoardgame } from './listSlice';
+import styles from './List.module.scss';
 
 const List = () => {
   const dispatch = useDispatch();
   const boardgames = useSelector(state => state.list.boardgames)
   const [newBoardgame, setNewBoardgame] = useState('')
 
-  const handleButton = (event) => {
+  const handleAddButton = (event) => {
     dispatch(addBoardgame(newBoardgame));
     setNewBoardgame('');
   };
 
+  const handleDeleteButton = (boardgame) => {
+    dispatch(deleteBoardgame(boardgame))
+  }
+
   return (
-    <div>
-      <div>
-        <input onChange={(e) => setNewBoardgame(e.target.value)} value={newBoardgame}></input>
-        <button onClick={handleButton}>Add Boardgame</button>
+    <div className={styles.list}>
+      <div className={styles.list_add}>
+        <input className={styles.list_add_input} onChange={(e) => setNewBoardgame(e.target.value)} value={newBoardgame}></input>
+        <button className={styles.list_add_button} onClick={handleAddButton}>Add Boardgame</button>
       </div>
-      <ul>
-        {boardgames?.map((boardgame, index) => (<li key={index}>{boardgame}</li>))}
+      <ul className={styles.list_collection}>
+        {boardgames?.map((boardgame, index) => (
+        <div className={styles.list_collection_item} key={index}>
+          <li>{boardgame}</li>
+          <button onClick={() => handleDeleteButton(boardgame)}  className={styles.list_collection_item_delete}>X</button>
+        </div>
+          ))}
       </ul>
 
     </div>
