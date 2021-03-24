758fd1724f058cca710a6d9d57da34ec2fc7f194
Alex Pliakas <pliakasbenitez@gmail.com>
2021-03-24 14:21:35 +0100

    Navbar changes depending on user Signed status

--- boardgame-collection/src/components/navbar/Navbar.js
+++ boardgame-collection/src/components/navbar/Navbar.js
@@ -6,30 +6,37 @@ import { signOut } from '../../store/actions/authActions'
 import styles from './Navbar.module.scss';
 
 const Navbar = (props) => {
+  const { auth, signOut } = props
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbar_left}>
         <Link to='/' className={styles.navbar_left_logo}>LOGO</Link>
       </div>
-      <div className={styles.navbar_right}>
-        <Link to='/' className={styles.navbar_right_item}>Search</Link>
-        <Link to='/signin' className={styles.navbar_right_item}>Sign In</Link>
-        <Link to='/signup' className={styles.navbar_right_item}>Sign Up</Link>
-      </div>
-      <div className={styles.navbar_right}>
-        <Link to='/' className={styles.navbar_right_item}>Search</Link>
-        <Link to='/' className={styles.navbar_right_item}>My Collection</Link>
-        <Link to='/' className={styles.navbar_right_item}>Discover</Link>
-        <a onClick={props.signOut} className={styles.navbar_right_item}>Sign out</a> 
-      </div>
+
+      { auth.uid ? (
+        <div className={styles.navbar_right}>
+          <Link to='/' className={styles.navbar_right_item}>Search</Link>
+          <Link to='/' className={styles.navbar_right_item}>My Collection</Link>
+          <Link to='/' className={styles.navbar_right_item}>Discover</Link>
+          <Link onClick={signOut} className={styles.navbar_right_item}>Sign out</Link> 
+        </div>
+      ) : (
+        <div className={styles.navbar_right}>
+          <Link to='/' className={styles.navbar_right_item}>Search</Link>
+          <Link to='/signin' className={styles.navbar_right_item}>Sign In</Link>
+          <Link to='/signup' className={styles.navbar_right_item}>Sign Up</Link>
+        </div>
+      )}
+      
+      
     </nav>
   )
 };
 
 const mapStateToProps = (state) => {
-  console.log(state)
   return {
-
+    auth: state.firebase.auth
   }
 }
 
